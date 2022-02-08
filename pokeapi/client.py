b4c576f0c19b56c3cfb7d2f5c72631eb33ed7e8c
sarthak-py <shiva02939@gmail.com>
2022-02-08 08:53:59 +0530

    enhanced client

--- pokeapi/client.py
+++ pokeapi/client.py
@@ -9,8 +9,7 @@ BASE_URL = "https://pokeapi.co/api/v2/pokemon/"
 
 class Client:
     def __init__(self) -> None:
-        self.cache = {} # url : res format
-
+        self.cache = {}  # url : res format
 
     async def get_pokemon(self, pokemon: typing.Union[int, str] = None) -> Pokemon:
         """
@@ -19,15 +18,18 @@ class Client:
         if not pokemon:
             pokemon = random.randint(1, 500)
         req_url = f"{BASE_URL}{pokemon}"
-        cached_data =self.cache.get(req_url)
-        if cached_data : return cached_data
+        cached_data = self.cache.get(req_url)
+        if cached_data:
+            return cached_data
         async with aiohttp.ClientSession() as session:
-            try : 
+            try:
                 response = await session.get(req_url)
             except asyncio.TimeoutError:
-                raise asyncio.TimeoutError('Unable to connect to the API (https://pokeapi.co/)')
+                raise asyncio.TimeoutError(
+                    "Unable to connect to the API (https://pokeapi.co/)"
+                )
         if response.status == 404:
-            raise 
-        data = await  response.json()
+            raise BaseException("No pokemon with Name or ID {} found".format(pokemon))
+        data = await response.json()
         self.cache[req_url] = data
         return Pokemon(data)
