1525d4e2182c20f7f2c3fa98f15711617e89fd8b
sarthak-py <shiva02939@gmail.com>
2022-02-11 20:10:07 +0530

    made cache optional!

--- pokeapi/client.py
+++ pokeapi/client.py
@@ -4,9 +4,9 @@ from typing import Union
 
 import aiohttp
 
-from.cache import CacheImpl
-from.pokemon import Pokemon
-from.errors import PokemonNotFound, ConnectionError
+from .cache import CacheImpl
+from .pokemon import Pokemon
+from .errors import PokemonNotFound, ConnectionError
 
 BASE_URL = "https://pokeapi.co/api/v2/pokemon/"
 
@@ -15,22 +15,23 @@ class Client:
     """The Client class with all the methods related to Base API.
 
     This class is made to define a object which would send requests and recieve raw data from the API.
-    
+
     Parameters:
     -----------
 
     cache: :class:`bool`
         A bool identifying if the Client should cache response from urls on every API requests or not.
         True will cache the data, False won't (defaulted to True).
-    
+
     Attributes:
     -----------
 
     cache: :class:`CacheImpl`
         The cache class with all the cached data.
-    
+
     """
-    def __init__(self , cache : bool=True) -> None:
+
+    def __init__(self, cache: bool = True) -> None:
         self.cache = None
         if cache:
             self.cache = CacheImpl(self)
@@ -38,7 +39,7 @@ class Client:
     async def get_pokemon(self, pokemon: Union[int, str] = None) -> Pokemon:
         """A method used to get the Pokémon.
 
-        This method is used to get a `Pokemon` object either from the cache or the API.
+        This method is used to get a `Pokémon` object either from the cache or the API.
 
         Parameters:
         -----------
@@ -46,19 +47,20 @@ class Client:
         pokemon: :class:`Union[int, str]`
             The id or name of the Pokémon to get data of.
             Gets a random Pokemon if no value provided
-        
+
         Returns:
         --------
 
-        :class:`Pokemon` 
+        :class:`Pokemon`
         """
 
         if not pokemon:
             pokemon = random.randint(1, 500)
         req_url = f"{BASE_URL}{pokemon}"
-        cached_data = self.cache.pokemon_cache.get(pokemon)
-        if cached_data:
-            return cached_data
+        if self.cache :
+            cached_data = self.cache.pokemon_cache.get(pokemon)
+            if cached_data:
+                return cached_data
 
         async with aiohttp.ClientSession() as session:
 
@@ -71,7 +73,10 @@ class Client:
             raise PokemonNotFound(pokemon)
 
         data = await response.json()
-        self.cache.pokemon_cache[data['id']] = data
-        self.cache.pokemon_cache[data['name']] = data
+        if self.cache:
+            self.cache.pokemon_cache[data["id"]] = data
+            self.cache.pokemon_cache[data["name"]] = data
+            self.cache.sprites_cache_impl[data['id']] = data['sprites']
+            self.cache.sprites_cache_impl[data['name']] = data['sprites']
 
         return Pokemon(self, data)
