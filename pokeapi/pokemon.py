e34dd2a38dc21fb3313c29fa4b081fe611923afe
sarthak-py <shiva02939@gmail.com>
2022-02-11 08:23:35 +0530

    initialiased pokemon class

--- pokeapi/pokemon.py
+++ pokeapi/pokemon.py
@@ -1,11 +1,15 @@
+
+
 class Pokemon:
     """
     Class for a Pokémon with most of the information about the Pokémons avaiable as an attribute.
     Complete info can be accessed with `Pokemon.raw` as an dictionary ( json response from the API ).
     """
 
-    def __init__(self, data: dict) -> None:
+    def __init__(self,client,  data: dict) -> None:
         self.data = data
+        self._client = client
+    
 
     def __str__(self) -> str:
         return self.name
