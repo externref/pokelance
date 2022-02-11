4fc4b3dc2bd020be256deeaa0599ae72b198a6da
sarthak-py <shiva02939@gmail.com>
2022-02-11 20:10:22 +0530

    custom errors

--- pokeapi/errors.py
+++ pokeapi/errors.py
@@ -1,13 +1,22 @@
 import asyncio
 import aiohttp
 
+
 class PokeAPIException(Exception):
     ...
 
+
 class PokemonNotFound(PokeAPIException):
     def __init__(self, entry) -> None:
-        super().__init__("No Pokémon with name or ID \"{}\" was found. Check if your provided the correct arguments and the website is online.".format(entry))
+        super().__init__(
+            'No Pokémon with name or ID "{}" was found. Check if your provided the correct arguments and the website is online.'.format(
+                entry
+            )
+        )
+
 
 class ConnectionError(aiohttp.ClientConnectionError, PokeAPIException):
-    def __init__(self) ->None:
-        super() .__init__('Unable to connect to the API, make sure you have a stable internet connection.')   
+    def __init__(self) -> None:
+        super().__init__(
+            "Unable to connect to the API, make sure you have a stable internet connection."
+        )
