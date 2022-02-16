79abaaff7dbad3cfcda32645b217eb78e86d7e66
sarthak-py <shiva02939@gmail.com>
2022-02-16 16:52:09 +0530

    documented errors

--- discmon/errors.py
+++ discmon/errors.py
@@ -6,6 +6,7 @@ class PokeAPIException(Exception):
 
 
 class PokemonNotFound(PokeAPIException):
+    """Raised when no Pokémon with the ID/name provided in `Client.get_pokemon` is found."""
     def __init__(self, entry) -> None:
         super().__init__(
             'No Pokémon with name or ID "{}" was found. Check if your provided the correct arguments and the website is online.'.format(
@@ -15,6 +16,7 @@ class PokemonNotFound(PokeAPIException):
 
 
 class ConnectionError(aiohttp.ClientConnectionError, PokeAPIException):
+    """Raised when the Client is unable to connect with the API."""
     def __init__(self) -> None:
         super().__init__(
             "Unable to connect to the API, make sure you have a stable internet connection."
