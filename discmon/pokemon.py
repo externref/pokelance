e4f4930875c74fed4068ef840a0612e2a305cf19
sarthak-py <shiva02939@gmail.com>
2022-02-20 10:35:01 +0530

    new branch,

--- discmon/pokemon.py
+++ discmon/pokemon.py
@@ -4,11 +4,31 @@ from .sprites import Sprite
 class Pokemon:
     """Class for a Pokémon with most of the information about the Pokémons available as an attribute.
     Complete info can be accessed with :class:`Pokemon.raw` as a dictionary ( JSON response from the API ).
+
+    Attributes
+    ----------
+        id: :class:`int`
+            ID of the Pokémon
+        name: :class:`str`
+            Name of the Pokémon
+        height: :class:`int`
+            Height of the Pokémon
+        weight: :class:`int`
+            Weight of the Pokémon
+        order: :class:`int`
+            Pokemon Order
     """
+
+    id: int
+    name: str
+    height: int
+    weight: int
+    order: int
 
     def __init__(self, client, data: dict) -> None:
         self.data = data
         self._client = client
+        self.add_basic_attrs(data)
 
     def __str__(self) -> str:
         return self.name
@@ -18,15 +38,12 @@ class Pokemon:
         """The raw JSON data about the Pokémon got from the API's response"""
         return self.data
 
-    @property
-    def id(self) -> int:
-        """The ID of the Pokémon"""
-        return self.data["id"]
-
-    @property
-    def name(self) -> str:
-        """Name of the Pokémon"""
-        return self.data["name"]
+    def add_basic_attrs(self, data) -> None:
+        self.name = data["name"]
+        self.id = data["id"]
+        self.height = data["height"]
+        self.weight = data["weight"]
+        self.order = data["order"]
 
     @property
     def sprites(self) -> Sprite:
