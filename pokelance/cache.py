ef32b6951b0dac057d6deb5fb3d6510ee1535ab6
sarthak-py <shiva02939@gmail.com>
2022-02-24 22:50:55 +0530

    update `Cache` and `Sprite` objects

--- pokelance/cache.py
+++ pokelance/cache.py
@@ -1,4 +1,4 @@
-from typing import Union, Optional, Dict
+from typing import Any, Union, Optional, Dict
 
 from .sprites import Sprite
 from .pokemon import Pokemon
@@ -19,7 +19,7 @@ class Cache:
     """
 
     def __init__(self, client) -> None:
-        self._pokemon_cache_data: Dict[str, Union[Dict, str, int, bool, None]] = {}
+        self._pokemon_cache_data: Dict[str, Any] = {}
         self._sprites_cache_data: Dict[str, Union[str, Dict]] = {}
         self.client = client
 
@@ -35,12 +35,18 @@ class Cache:
 
     def get_pokemon(self, identity: Union[int, str]) -> Optional[Pokemon]:
         """Get's the cached :class:`.Pokemon` for the `identity` entered."""
-        return Pokemon(self.pokemon_cache.get(str(identity))) if self.pokemon_cache.get(str(identity)) else None
+        return (
+            Pokemon(self.pokemon_cache.get(str(identity)))
+            if self.pokemon_cache.get(str(identity))
+            else None
+        )
 
     def get_sprite_for(self, pokemon: Union[Pokemon, int, str]) -> Optional[Sprite]:
         """Get's the cached :class:`.Sprite` for the pokemon entered."""
         if isinstance(pokemon, Pokemon):
             pokemon = pokemon.name
-        return Sprite(self._sprites_cache_data.get(str(pokemon))) if self._sprites_cache_data.get(str(pokemon)) else None
-
-
+        return (
+            Sprite(self._sprites_cache_data.get(str(pokemon)))
+            if self._sprites_cache_data.get(str(pokemon))
+            else None
+        )
